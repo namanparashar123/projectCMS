884dc6f25a5ef295ce8e36268803931ffc2251fa
namanparashar123 <45572583+namanparashar123@users.noreply.github.com>
2018-12-03 12:29:28 -0600

    Add files via upload

---
+++ error.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Oops...</title>
+    <link rel="stylesheet" href="main.css" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Allerta+Stencil" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Roboto+Condensed" rel="stylesheet">
+</head>
+<body>
+    <nav class="topnav">
+        <a href="https://www.marvel.com"><img src = "Images/indeximages.png" alt = "Avengers"></a>
+        <ul>
+            <li><a href = "index.php">About</a></li>
+            <li><a href = "characters.php">Characters</a></li>   
+            <li><a href = "share.php">Share</a></li>   
+            <li><a href = "#">Something</a></li>
+            <li><a href = "login.php">Login</a></li>
+        </ul>
+    </nav>
+    <h1>An error occured while processing your post.</h1>
+        <p>
+            Both the title and content must be at least one character. 
+        </p>
+        <p>
+            <a id="return" href="index.php">Return Home</a>
+        </p>
+</body>
+</html>
